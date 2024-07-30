3c90fb79d93b887f78bd049fa314d287b28a2db8
sunellopez <sunellopez@gmail.com>
2024-07-29 23:58:52 -0600

    v0.0.1 Se actualizó compilación

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.frontend',
   appName: 'app-frontend',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
